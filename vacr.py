a0ef7e7fb271e1ecc21579c2735ce0358986df1a
Merlijn Wajer <merlijn@wajer.org>
2010-08-02 01:11:12 +0200

    Small changes.

--- vacr.py
+++ vacr.py
@@ -7,6 +7,8 @@ import time
 #p = subprocess.Popen(['gst-launch-0.10', 'pulsesrc ! wavenc ! fdsink fd=1'], \
 #                stdout=subprocess.PIPE)
 
+ofile = 'test.wav'
+
 # Arecord just dumps the raw wav to stdout. We will use this
 # to read from with out wave module.
 p = subprocess.Popen(['arecord'], stdout=subprocess.PIPE)
@@ -14,19 +16,26 @@ p = subprocess.Popen(['arecord'], stdout=subprocess.PIPE)
 # Open the pipe.
 f = wave.open(p.stdout)
 
-# Open file we will write to.
-o = wave.open('test.wav', 'w')
-o.setparams((1, 1, 8000, 0, 'NONE', 'not compressed'))
-o.setnchannels(1)
-
-high, lasthigh = False, False
-
 # Print audio set up
 print f.getparams()
 
+nchan, sampwidth, framerate, nframes, comp, compname = f.getparams()
+print 'Sample / s:', framerate
+
+if framerate != 8000 or nchan != 1:
+    print 'Format currently supported. Only supported format is 8000 frames and 1
+channel'
+
+
+# Open file we will write to.
+o = wave.open(ofile, 'w')
+o.setparams((nchan, sampwidth, framerate, 0, 'NONE', 'not compressed'))
+
+high, lasthigh = False, False
+
 while True:
     # Read 1 second
-    a = f.readframes(8000)
+    a = f.readframes(framerate)
     b = [ord(x) for x in a]
     _min, _max = min(b), max(b)
 
