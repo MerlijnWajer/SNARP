b45ceed546c2d085dab6c3e50e0207ae8bf14492
Merlijn Wajer <merlijn@wizzup.org>
2011-02-16 00:42:21 +0100

    SNARP: Creating a new (simple) structure.
    Now uses cStringIO instead of a direct file. It probably can't run for too
    long since it never flushes the cStringIO buffer. It should probably flush it to
    disk every X minutes.
    
    Also moved to a more OOP approach.

--- snarp.py
+++ snarp.py
@@ -3,57 +3,100 @@
 import subprocess
 import wave
 import time
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
+
+
+class SinkInput(object):
+    """
+        TODO: arecord will keep on running. We need to drop frames when it's
+        paused or when just not being read. Perhaps only keep recent frames?
+        Also, arecord is a rather primitive way to read input. Might want to
+        switch to gstreamer.
+    """
+
+    def __init__(self):
+        # Arecord just dumps the raw wav to stdout. We will use this
+        # to read from with out wave module.
+        self.p = subprocess.Popen(['arecord'], stdout=subprocess.PIPE)
+        # Open the pipe.
+        self.f = wave.open(self.p.stdout)
+        self.nchan, self.sampwidth, self.framerate, self.nframes, self.comp, \
+                self.compname = self.f.getparams()
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def get_output(self):
+        return self.f
+
+class NoiseFilter(object):
+    def __init__(self):
+        pass
+
+class BufferedClassFile(object):
+    def __init__(self):
+        self.s = StringIO.StringIO()
+
+    def get_stream(self):
+        return self.s
 
 #p = subprocess.Popen(['gst-launch-0.10', 'pulsesrc ! wavenc ! fdsink fd=1'], \
 #                stdout=subprocess.PIPE)
 
 ofile = 'test.wav'
 
-# Arecord just dumps the raw wav to stdout. We will use this
-# to read from with out wave module.
-p = subprocess.Popen(['arecord'], stdout=subprocess.PIPE)
 
-# Open the pipe.
-f = wave.open(p.stdout)
+inp = SinkInput()
 
-# Print audio set up
-print f.getparams()
+# Print audio setup
+print inp.f.getparams()
 
-nchan, sampwidth, framerate, nframes, comp, compname = f.getparams()
-print 'Sample / s:', framerate
+print 'Sample / s:', inp.framerate
 
-if framerate != 8000 or nchan != 1:
+if inp.framerate != 8000 or inp.nchan != 1:
     print 'Format currently not supported.'\
             'Only supported format is 8000 frames and 1 channel'
 
+buf = BufferedClassFile()
 
 # Open file we will write to.
-o = wave.open(ofile, 'w')
-o.setparams((nchan, sampwidth, framerate, 0, 'NONE', 'not compressed'))
+o = wave.open(buf.get_stream(), 'w')
+o.setparams((inp.nchan, inp.sampwidth, inp.framerate, 0, 'NONE', \
+            'not compressed'))
 
 high, lasthigh = False, False
 
-while True:
-    # Read 1 second
-    a = f.readframes(framerate)
-    b = [ord(x) for x in a]
-    _min, _max = min(b), max(b)
+    
+try:
+    while True:
+        # Read 1 second
+        a = inp.f.readframes(inp.framerate)
+        b = [ord(x) for x in a]
+        _min, _max = min(b), max(b)
+    
+        # Print bounds
+        print 'min', _min
+        print 'max', _max
+    
+        # TODO: The gate should obviously be configurable.
+        if _max > 135 and _min < 120:
+            high = True
+        else:
+            high = False
+    
+        # Write always if either is True.
+        if lasthigh or high:
+            o.writeframes(a)
+    
+        lasthigh = high
 
-    # Print bounds
-    print 'min', _min
-    print 'max', _max
-
-    # TODO: The gate should obviously be configurable.
-    if _max > 135 and _min < 120:
-        high = True
-    else:
-        high = False
-
-    # Write always if either is True.
-    if lasthigh or high:
-        o.writeframes(a)
-
-    lasthigh = high
-
-f.close()
-o.close()
+finally:
+    inp.f.close()
+    o.close()
+    print len(buf.get_stream().getvalue())
