81a1d9f9badd30e8984bba21fef7c98e911f014e
Christopher H. Casebeer <christopher@chc.name>
2013-09-11 19:33:49 -0400

    Shorter chunk timings. Update tests to match.
    Set the timings back down to much more agressive, more responsive
    values. Chunks are now 100 ms long, pre- and post-roll are 200 ms
    and 100 ms, respectively, and the audible segment termination
    hysteresis window is 1000 ms (whereas the original SNARP code
    had an unspecified effective hysteresis window of 3000 ms).
    
    This lets us clip audio segments much closer to the actual audible
    portions while still being reasonably generous with not cutting
    apart short bursts of silence with a longer audible segment.
    
    Update tests to use expected file lengths for the new, shorter
    chunk sizes.
    
    Remove hardcoded SILENCE_MIN and SILENCE_MAX since we're now using
    deltas.

--- test/test_snarp.py
+++ test/test_snarp.py
@@ -5,12 +5,15 @@ import os
 
 OUTPUT_FILENAME = "/tmp/output.wav"
 
+def assert_eq(a, b):
+	assert a == b, "{0!r} != {1!r}".format(a, b)
+
 def test_basic_functionality():
 	inputs = [
-		("test/data/generated-beeps-44k-16bit-1ch.wav", 262395, 174195),
-		("test/data/generated-beeps-44k-8bit-1ch.wav", 262395, 174195),
-		("test/data/generated-beeps-22k-16bit-1ch.wav", 131198, 87098),
-		("test/data/generated-beeps-22k-8bit-1ch.wav", 131198, 87098),
+		("test/data/generated-beeps-44k-16bit-1ch.wav", 262395, 55125),
+		("test/data/generated-beeps-44k-8bit-1ch.wav", 262395, 55125),
+		("test/data/generated-beeps-22k-16bit-1ch.wav", 131198, 27563),
+		("test/data/generated-beeps-22k-8bit-1ch.wav", 131198, 27563),
 	]
 	for filename, expected_input_frames, expected_output_frames in inputs:
 		check_basic_functionality(filename, expected_input_frames, expected_output_frames)
@@ -18,8 +21,8 @@ def test_basic_functionality():
 def check_basic_functionality(filename, expected_input_frames, expected_output_frames):
 	with open(filename, "rb") as input:
 		with open(OUTPUT_FILENAME, "wb+") as output:
-			with snarp.silence_limits(120, 135):
-				snarp.remove_silences(input, output)
+		#	with snarp.silence_limits(120, 135):
+			snarp.remove_silences(input, output)
 
 			input.seek(0)
 			output.seek(0)
@@ -27,13 +30,14 @@ def check_basic_functionality(filename, expected_input_frames, expected_output_f
 			wave_input = wave.open(input, 'rb')
 			wave_output = wave.open(output, 'rb')
 
+			print("Filename: {0}".format(filename))
 			print("Input is {0} frames long".format(wave_input.getnframes()))
 			print("Output is {0} frames long".format(wave_output.getnframes()))
 
 			# Given that we're using the correct test input file
-			assert(wave_input.getnframes() == expected_input_frames)
+			assert_eq(wave_input.getnframes(), expected_input_frames)
 			# Ensure we removed a consistent amount of silence
-			assert(wave_output.getnframes() == expected_output_frames)
+			assert_eq(wave_output.getnframes(), expected_output_frames)
 
 	# cleanup, delete temp output file
 	os.unlink(OUTPUT_FILENAME)
